0e50dfcb446ab5f2d52ccbf1e8065ad57f41d247
DerVerruckte <michael.heinhold@gmail.com>
2021-10-03 09:23:46 -0600

    notes link works

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001
+
+const app = express();
+
+//parse incoming string or array data
+app.use(express.urlencoded({extended:true}));
+//parse incoming json data
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use('/', htmlRoutes);
+// app.use('/api', apiRoutes);
+
+app.listen(PORT, () =>{
+    console.log('Server now live on port 3001!');
+});
