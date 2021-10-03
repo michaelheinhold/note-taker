509156735d32882475a873f57430676d42611eb0
DerVerruckte <michael.heinhold@gmail.com>
2021-10-03 12:21:08 -0600

    finished work

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
 const db = require('../../db/db.json');
 const path = require('path');
+const { createNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
-    let response = db;
+    let response = db.savedNotes;
     res.send(response);
 });
+
+router.post('/notes', (req, res) =>{
+    if(!db.length){
+        db.length = 0;
+    }
+    req.body.id = db.length.toString();
+
+    const newNote = createNote(req.body, db.savedNotes);
+    res.json(newNote);
+})
 
 module.exports = router;
